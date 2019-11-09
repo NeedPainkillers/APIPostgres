49c6d4c6ebd990c7090f12ea75aff015cd1adc4f
NeedPainkillers <46500324+NeedPainkillers@users.noreply.github.com>
2019-11-09 10:54:16 +0300

    Initial commit
    Template UOW context implemented

---
+++ Kulkov/Controllers/WeatherForecastController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kulkov.Data;
+using Kulkov.UOW;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Kulkov.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public WeatherForecastController()
+        { 
+
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Account>> Get()
+        {
+            var template = new Template("Temp");
+            await template.TestConnection();
+            return await template.Accounts;
+        }
+    }
+}
---
+++ Kulkov/UOW/Template.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using Npgsql;
+using Kulkov.Data;
+
+namespace Kulkov.UOW
+{
+    public class Template
+    {
+        string _connString = "Host=host.docker.internal;Username=postgres;Password=sdfl234;Database=taskdb";
+        private readonly NpgsqlConnection _conn;
+
+        public Template(string connectionString)
+        {
+            //_connString = connectionString;
+            _conn = new NpgsqlConnection(_connString);
+        }
+
+        public Task<IEnumerable<Account>> Accounts
+        {
+            get
+            {
+                return getAccountsInternal();
+            }
+        }
+
+        private async Task<IEnumerable<Account>> getAccountsInternal()
+        {
+            await _conn.OpenAsync();
+
+            List<Account> Response = new List<Account>();
+            // Retrieve all rows
+            await using (var cmd = new NpgsqlCommand("SELECT t.*, CTID FROM public.account t ORDER BY user_id DESC", _conn))
+            await using (var reader = await cmd.ExecuteReaderAsync())
+                while (await reader.ReadAsync())
+                {
+                    Response.Add(new Account()
+                    {
+                        user_id = Int32.Parse(reader.GetValue(0).ToString()),
+                        username = reader.GetValue(1).ToString(),
+                        password = reader.GetValue(2).ToString(),
+                        email = reader.GetValue(3).ToString(),
+                        created_on = reader.GetValue(4).ToString(),
+                        last_login = reader.GetValue(5).ToString()
+                    });
+                }
+            return Response;
+        }
+
+        public async Task<IEnumerable<string>> TestConnection()
+        {
+            await using var conn = new NpgsqlConnection(_connString);
+            await conn.OpenAsync();
+
+            // Insert some data
+            await using (var cmd = new NpgsqlCommand("INSERT INTO taskdb.public.account ( username, password, email, created_on, last_login) VALUES ((@p), 'somePwd', 'Test@email.example', now(), now());", conn))
+            {
+                cmd.Parameters.AddWithValue("p", "Hello world");
+                await cmd.ExecuteNonQueryAsync();
+            }
+
+            List<string> Response = new List<string>();
+            // Retrieve all rows
+            await using (var cmd = new NpgsqlCommand("SELECT username FROM account", conn))
+            await using (var reader = await cmd.ExecuteReaderAsync())
+                while (await reader.ReadAsync())
+                    Response.Add(reader.GetString(0));
+            return Response;
+
+        }
+    }
+}
